648dc8eb060aafc501f98f2e7b032dc930e8c3eb
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-07-17 23:45:51 +0900

    알고리즘 풀이 중간

---
+++ src/practice.java
@@ -0,0 +1,15 @@
+public class practice {
+
+    public static void main(String[] args) {
+        int result1 = add(100 ,200);
+        double result2 = add(100 ,300);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+}
