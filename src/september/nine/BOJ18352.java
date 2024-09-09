78a9199e128b57266f7d7bc00e99b0a96fde4a27
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-09-09 23:23:26 +0900

    BOJ18352 풀이 중간

---
+++ src/september/nine/BOJ18352.java
@@ -0,0 +1,59 @@
+package september.nine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ18352 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N,M,K,X;
+    private static int[][] dist;
+    private static List<Integer> answer = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        dist = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            dist[a][b] = 1;
+        }
+
+        dijkstra();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dist[i][j] == K) {
+
+                }
+            }
+        }
+    }
+
+    static void dijkstra() {
+        for(int k = 0; k < N; k++) {
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+}
