11ff80cc8ef683d53a64b0d86a3d2692eb764c6c
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-09-10 17:31:18 +0900

    BOJ 문제 풀이

--- src/september/nine/BOJ18352.java
+++ src/september/nine/BOJ18352.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ18352 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int N,M,K,X;
-    private static int[][] dist;
+    private static int[][] graph;
+    private static int[] dist;
     private static List<Integer> answer = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,41 +21,28 @@ public class BOJ18352 {
         K = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
-        dist = new int[N][N];
-
+        graph = new int[N][N];
+        dist = new int[N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                dist[i][j] = Integer.MAX_VALUE;
+                graph[i][j] = Integer.MAX_VALUE;
             }
         }
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            dist[a][b] = 1;
-        }
 
-        dijkstra();
-
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                if (dist[i][j] == K) {
-
-                }
+                int a = Integer.parseInt(st.nextToken()) - 1;
+                int b = Integer.parseInt(st.nextToken()) - 1;
+                graph[a][b] = 1;
             }
         }
     }
 
     static void dijkstra() {
-        for(int k = 0; k < N; k++) {
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    if(dist[i][k] + dist[k][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
-        }
+
     }
 
 }
