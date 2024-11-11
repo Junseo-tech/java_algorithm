3a32df2a690a819cd583d50c81f21a16c28e2fdf
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-11-12 01:27:53 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 500 ms, Memory: 48840 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,68 +1,79 @@
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int V,E;
-    static int[] parents;
+    private static int[] parents;
+
     static class Edge implements Comparable<Edge> {
         int start, end, weight;
+
         public Edge(int start, int end, int weight) {
             this.start = start;
             this.end = end;
             this.weight = weight;
         }
-        @Override
         public int compareTo(Edge o) {
             return this.weight - o.weight;
         }
     }
 
-    private static void makeSet(){
+    private static void makeSet() {
         parents = new int[V];
         for (int i = 0; i < V; i++) {
-            parents[i] = -1;
+            parents[i] = i;
         }
     }
 
-    private static int findSet(int v){
-        if(parents[v] < 0) return v;
-        return parents[v] = findSet(parents[v]);
+    private static int find(int v) {
+        if(parents[v] == v) return v;
+        return parents[v] = find(parents[v]);
     }
 
     private static boolean union(int a, int b) {
-        int rootA = findSet(a);
-        int rootB = findSet(b);
-        if(rootA == rootB) return false;
-        if(rootA < rootB) parents[rootB] = rootA;
-        else parents[rootA] = rootB;
+        int parentA = find(a);
+        int parentB = find(b);
+        if(parentA == parentB) return false;
+        if(parentA > parentB) parents[parentA] = parentB;
+        else parents[parentB] = parentA;
         return true;
     }
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int V,E;
+    private static Edge[] edges;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-        Edge[] edges = new Edge[E];
+        edges = new Edge[E];
+
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            edges[i] = new Edge(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()));
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int weight = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(a, b, weight);
         }
+
         Arrays.sort(edges);
         makeSet();
 
         int cnt = 0;
         int cost = 0;
-        for(Edge edge : edges){
-            if(union(edge.start, edge.end)){
-                cost += edge.weight;
-                if(++cnt == V-1) break;
+        for(Edge e : edges) {
+            if(union(e.start, e.end)){
+                cost += e.weight;
+                if(++cnt == V-1) {
+                    break;
+                }
             }
         }
+
         System.out.println(cost);
     }
 }
