0843bf7cd068f2c9cf8160408fb0e41e57f24fbf
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2025-07-28 20:29:00 +0900

    [Silver III] Title: N과 M (7), Time: 720 ms, Memory: 265220 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N, M;
+    private static int[] arr;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 개수
+        M = Integer.parseInt(st.nextToken()); // 길이
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        combination(0, new int[M], 0);
+        System.out.println(sb.toString());
+    }
+
+    private static void combination(int depth, int[] temp, int start) {
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(temp[i] + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+                temp[depth] = arr[i];
+                combination(depth + 1, temp, i);
+            }
+        }
+    }
+
