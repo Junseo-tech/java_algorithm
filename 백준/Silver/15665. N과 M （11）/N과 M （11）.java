67eaf5f6a9234f3cf49dbbe7e8a982c1700a7b75
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2025-07-29 09:43:51 +0900

    [Silver II] Title: N과 M (11), Time: 324 ms, Memory: 33916 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N, M;
+    private static int[] arr;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 개수
+        M = Integer.parseInt(st.nextToken()); // 길이
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        permutation(0, new int[M]);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void permutation(int depth, int[] temp) {
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(temp[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int prev = 0;
+        for(int i = 0; i < N; i++) {
+            if(prev != arr[i]) {
+                temp[depth] = arr[i];
+                prev = arr[i];
+                permutation(depth+1, temp);
+            }
+        }
+    }
+}
