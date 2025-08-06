f241e3a222fdb13c3f7a7917858f8150234ef279
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2025-08-06 10:29:43 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 18088 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -10,34 +10,33 @@ public class Main {
     private static int N, K; // N : 만들어야 하는 랜선 수, K : 가지고 있는 랜선, 길이 제각각
     private static int[] lines;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static long answer;
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
         lines = new int[K];
+        int maxNum = Integer.MIN_VALUE;
 
         for(int i = 0; i < K; i++) {
             lines[i] = Integer.parseInt(br.readLine());
+            maxNum = Math.max(maxNum, lines[i]);
         }
 
-        Arrays.sort(lines);
         // 구하는 답은 최대 랜선의 길이, 종료되는 조건은 N 이상일 때
         // 길이를 잡고 안되면 줄이는 방식으로 가야 할 거 같음. N 보다 작으면 값을 줄이는. mid가 답이되는 형식의 이분 탐색 ?
         // 457 539 743 802
 
-        answer = 1;
-        binarySearch(0, lines[K - 1]);
-        System.out.println(answer);
+
+        long result = binarySearch(1, maxNum);
+        System.out.println(result);
 
     }
 
-    private static void binarySearch(long start, long end) {
+    private static long binarySearch(long start, long end) {
+        long result = 0;
         while(start <= end) {
-            long mid = (start + end) / 2; // ex. 629
-
-            if(mid <= 0) return;
+            long mid = start + (end - start) / 2; // ex. 629
 
             int temp = 0;
             for(int i = 0; i < K; i++) {
@@ -47,10 +46,10 @@ public class Main {
             if(temp < N) {
                 end = mid - 1;
             } else { // 현재 갯수가 N개
-                answer = Math.max(answer, mid);
+                result = mid;
                 start = mid + 1;
             }
-
         }
+        return result;
     }
 }
