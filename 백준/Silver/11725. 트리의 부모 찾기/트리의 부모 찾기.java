2a103d5c57a26ef2b73fd8f4ae7bb8d1a69d7d29
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2026-03-14 13:54:00 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 612 ms, Memory: 66280 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,14 +4,17 @@ import java.io.*;
 public class Main {
     private static int N;
     private static List<List<Integer>> graph;
-    private static Map<Integer, Integer> result;
+   // private static Map<Integer, Integer> result;
+    private static int[] result;
     private static boolean[] visited;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String args[]) throws IOException {
-        N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine()); // N (2 ≤ N ≤ 100,000)
         graph = new ArrayList<>();
-        result = new TreeMap<Integer, Integer>();
+     //   result = new TreeMap<Integer, Integer>();
         visited = new boolean[N + 1];
+        result = new int[N + 1];
 
         for(int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
@@ -29,19 +32,24 @@ public class Main {
         dfs(1);
 
         StringBuilder sb = new StringBuilder();
-        for(Map.Entry<Integer, Integer> t : result.entrySet()) {
-            sb.append(t.getValue()).append("\n");
+//        for(Map.Entry<Integer, Integer> t : result.entrySet()) { // O(N)
+//            sb.append(t.getValue()).append("\n");
+//        }
+
+        for(int i = 2; i <= N; i++) {
+            sb.append(result[i]).append("\n");
         }
 
         System.out.println(sb);
 
     }
 
-    private static void dfs(int start) {
+    private static void dfs(int start) { // O(V + E) 인데 V == N, E == N-1 이니까 O(N)
         visited[start] = true;
         for(int temp : graph.get(start)) {
             if(!visited[temp]) {
-                result.putIfAbsent(temp, start);
+                //result.putIfAbsent(temp, start); // O(logN)
+                result[temp] = start;
                 dfs(temp);
             }
         }
