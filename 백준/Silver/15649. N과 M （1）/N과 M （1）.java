2a1b8c8db14ad053d393b077e952ced83f69297e
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2026-03-17 14:05:43 +0900

    [Silver III] Title: N과 M (1), Time: 256 ms, Memory: 23284 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     private static int N,M;
-    private static int[] arr;
-    private static StringBuilder sb = new StringBuilder();
+    private static int[] nums;
+    private static List<int[]> answer;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
+        N = Integer.parseInt(st.nextToken()); // N 까지
+        M = Integer.parseInt(st.nextToken()); // M개 고르기
+        answer = new ArrayList<>();
 
-        dfs(0,0);
-        System.out.println(sb.toString());
-    }
+        nums = new int[N+1];
 
-    private static void dfs(int depth, int visited) {
-        if(depth == M) {
-            for(int num : arr) {
+        for(int i = 1; i<=N; i++) {
+            nums[i] = i;
+        }
+
+        permutation(0, new boolean[N + 1], new int[M]);
+        StringBuilder sb = new StringBuilder();
+        for(int[] temp : answer) {
+            for(int num : temp) {
                 sb.append(num).append(" ");
             }
             sb.append("\n");
-            return;
         }
 
-        for(int i = 0; i < N; i++) {
-            if((visited & (1 << i)) == 0) {
-                arr[depth] = i + 1;
-                dfs(depth + 1, visited | (1 << i));
-            }
+        System.out.println(sb);
+    }
+
+    private static void permutation(int depth, boolean[] visited, int[] temp) {
+        if(depth == M) {
+            answer.add(temp.clone());
+            return;
+        }
+        for(int i = 1; i <= N; i++) {
+           if(!visited[i]) {
+                visited[i] = true;
+                temp[depth] = nums[i];
+                permutation(depth + 1, visited, temp);
+                visited[i] = false;
+           }
         }
     }
+
 }
