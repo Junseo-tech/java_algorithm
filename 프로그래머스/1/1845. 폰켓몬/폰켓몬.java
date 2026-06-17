0a6d3041b50df4c1d6d61e40a3c66d06e8b32271
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2026-06-17 15:42:41 +0900

    [level 1] Title: 폰켓몬, Time: 2.26 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,23 +1,23 @@
-// 같은 종류의 폰켓몬은 같은 번호를 가진다
-// 최대한 다양한 폰켓몬을 가지길 바란다. 따라서 최대한 많은 종류의 폰켓몬을 포함해서 n/2마리를 선택한다. 
-// 방법을 찾고, 종류 번호의 개수를 return 한다.
-
+// N 마리 중에 N/2마리 => 최대한 다양한 폰켓몬 선택 
+// return => 포켓몬 종류 
 import java.util.*;
+
 class Solution {
-    public int solution(int[] nums) { // nums 길이 10,000이 최대 , 늘 짝수 
+    public int solution(int[] nums) {
         int answer = 0;
-        HashMap<Integer, Integer> pocket = new HashMap<>();
-        int goal = nums.length / 2;
+        int N = nums.length / 2;
+        HashMap<Integer, Integer> maps = new HashMap<>();
         
-        for(int i = 0; i < nums.length; i++) {
-            pocket.put(nums[i], pocket.getOrDefault(nums[i], 0) + 1); // 일단 몇개 있는지 채워
+        for(int num : nums) {
+            maps.put(num, maps.getOrDefault(num, 0) + 1);
         }
         
-        if(pocket.size() > goal) {
-            answer = goal;
+        if(N <= maps.size()) {
+            answer = N;
         } else {
-            answer = pocket.size();
+            answer = maps.size();
         }
+        
         return answer;
     }
 }
