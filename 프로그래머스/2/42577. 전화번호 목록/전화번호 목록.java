a16149da59225c5758bd72998652430267ae0701
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2026-06-18 15:30:40 +0900

    [level 2] Title: 전화번호 목록, Time: 219.12 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,52 +1,16 @@
+// 한 번호가 다른 번호의 접두어인 경우 => 접두어인 경우가 있으면 false, 아니면 true
 import java.util.*;
 
-class Node {
-    HashMap<Character, Node> child = new HashMap<>();
-    boolean end = false;
-    
-    public Node() {
-        this.child = child;
-        this.end = end;
-    }
-}
-
-class Trie {
-    Node root;
-    
-    public Trie() {
-        this.root = new Node();
-    }
-    
-    public boolean insert(String str) {
-        Node node = this.root;
-        
-        for(int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            
-            if(node.child.containsKey(c) && node.child.get(c).end) {
-                return false;
-            }
-            node.child.putIfAbsent(c, new Node());
-            node = node.child.get(c);
-        }
-        
-        if(!node.child.isEmpty()) { // 다 넣었는데 아래 있음 => 내가 더 짧은 경우
-            return false;
-        }
-        
-        node.end = true;
-        
-        return true;
-    }
-}
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Trie trie = new Trie();
-        for(int i = 0; i < phone_book.length; i++) {
-            if(!trie.insert(phone_book[i])) {
+        
+        Arrays.sort(phone_book);
+            
+        for(int i = 1; i < phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i - 1])) {
                 answer = false;
-                return answer;
+                break;
             }
         }
         
