2693b22a17b4023f140acdbf1ab97d76e187ba6d
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2026-07-15 22:09:21 +0900

    [level 2] Title: 모음 사전, Time: 8.01 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,33 +1,29 @@
-// 길이 5이하 
 class Solution {
-    private static int count;
-    private static int answer;
+    static char[] words = new char[]{'A', 'E', 'I', 'O', 'U'};
+    static int answer;
+    static int count;
     public int solution(String word) {
-        char[] alphabet = new char[] {'A', 'E', 'I', 'O', 'U'}; // 5
         answer = 0;
-        count = 0;
         
-        permutation(0, word, "", alphabet);
+        makeWord(word, "");
         return answer;
     }
     
-    private static void permutation(int length, String target, String word, char[] alpha) {
-        
-        if(!word.equals("")) { // 순서니까 A부터 세기 
-            count++;
+    
+    private void makeWord(String word, String temp) {
+        if(answer != 0) {
+            return;
         }
-        
-        if(target.equals(word)) { // 맞으면 return 
+        if(temp.equals(word)) {
             answer = count;
             return;
         }
         
-        if(answer > 0) return;
-        
-        if(length == 5) return; // 5넘으면 못만듦
+        if(temp.length() == 5) return;
         
         for(int i = 0; i < 5; i++) {
-            permutation(length + 1, target, word + alpha[i], alpha);
+            count++;
+            makeWord(word, temp + words[i]);
         }
     }
 }
